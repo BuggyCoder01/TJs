b005de6bb8fb6d6bbb610c541b32560ab9304b73
BuggyCoder01 <68791440+BuggyCoder01@users.noreply.github.com>
2022-05-26 13:57:23 +0300

    frankinstien101

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 import './style.css'
 import * as THREE from 'three'
 import { normalize } from 'gsap'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -13,6 +17,25 @@ const sizes = {
 // Scene
 const scene = new THREE.Scene()
 
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 4
+//cubeMesh.position.normalize(camera.position)
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+//lighting
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+light.position.set()
+
 // Object
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
 const cubeMaterial = new THREE.MeshBasicMaterial({
@@ -20,28 +43,42 @@ const cubeMaterial = new THREE.MeshBasicMaterial({
 })
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
 cubeMesh.position.x=1
-cubeMesh.position.y=-1.4
+cubeMesh.position.y=1.25
 cubeMesh.position.z=1.9
-
+cubeMesh.scale.set(0.1,2.5,0.45)
+//camera.lookAt(cubeMesh.position)
 scene.add(cubeMesh)
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 4
-//cubeMesh.position.normalize(camera.position)
-scene.add(camera)
+//model object
+const loader = new OBJLoader();
 
-//camera.lookAt(cubeMesh.position)
+loader.load('/models/Earth2K.obj', function (obj) {
 
-cubeMesh.scale.set(0.1,2.5,0.45)
+	scene.add( obj);
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+//controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//gridHelper
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper)
 
 //axesHelper
 const axesHelper = new THREE.AxesHelper()
 scene.add(axesHelper)
 
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+function animate() {
+    requestAnimationFrame(animate);
+    // controls.update();
+    renderer.render(scene, camera);
+  }
+  animate();
+
+
